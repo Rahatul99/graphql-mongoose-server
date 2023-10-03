0a9b839e840729a395a10ba6de706b6ce7298d01
rahatul <rahatul99@gmail.com>
2023-10-03 11:51:12 +0600

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+export const MONGO_URI =
+  "mongodb+srv://graphql:kM21NqC9WYDggkQ6@cluster0.jp6ok1r.mongodb.net/?retryWrites=true&w=majority";
+
+export const JWT_SECRET = "jhdsafhsaiof";
